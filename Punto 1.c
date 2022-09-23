cc177a919d27cb9d2a6537894656ad6bdd0d7de3
Marwys <114246393+Marwys@users.noreply.github.com>
2022-09-23 10:45:48 -0500

    Update Punto 1.c

--- Punto 1.c
+++ Punto 1.c
@@ -56,12 +56,15 @@ ANSELH = 0x00; //Entradas digitales.
         
         //La constante c nos permitira cambiar los modos del PIC para el resto de los modos del led.
         
-        if(c == 0 ){
-            if(RE0 == 0){
-            
+        if(c == 0 )
+        {
+            if(RE0 == 0)
+            {
+                
                 __delay_ms(1000);
             
-                if(RE0 == 0){
+                    if(RE0 == 0)
+                    {
                 
                     RA0 = !RA0;
                     __delay_ms(500);
@@ -96,8 +99,8 @@ ANSELH = 0x00; //Entradas digitales.
                     RE2 = !RE2;
                     __delay_ms(500);
                     
-        }
-        }
+                   }
+            }
         }
         
        if(c == 1){
@@ -179,25 +182,25 @@ ANSELH = 0x00; //Entradas digitales.
                     RE2 = !RE2;
                     __delay_ms(500);
                     RA0 = !RA0;
-                    RE2 = !RE2;//4ta
+                    RE2 = !RE2;
                     __delay_ms(500);
                     RA1 = !RA1;
                     RE1 = !RE1;
                     __delay_ms(500);
                     RA1 = !RA1;
-                    RE1 = !RE1;//3ra
+                    RE1 = !RE1;
                     __delay_ms(500);
                     RA2 = !RA2;
                     RA5 = !RA5;
                     __delay_ms(500);
                     RA2 = !RA2;
-                    RA5 = !RA5; //2da
+                    RA5 = !RA5; 
                     __delay_ms(500);
                     RA3 = !RA3;
                     RA4 = !RA4;
                     __delay_ms(500);
                     RA3 = !RA3;
-                    RA4 = !RA4; //1ra
+                    RA4 = !RA4; 
                     __delay_ms(500);
                 }
                 }
